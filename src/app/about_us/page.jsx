8a3b27de7454fb29daaec3693c947bdbaddbdf6c
Samuel-joseph05 <samuela05022004@gmail.com>
2025-12-06 14:48:02 +0530

    feat: add About Us page component

---
+++ src/app/about_us/page.jsx
@@ -0,0 +1,18 @@
+"use client";
+import { useRouter } from "next/navigation";
+
+export default function Aboutus(){
+
+      const router = useRouter();
+
+return(
+<section>
+    <div className="text-center">
+        <h1>About Us</h1>
+    </div>
+</section>
+);
+
+
+
+}
