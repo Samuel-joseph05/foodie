278ea0dcd8308ece17ba5b371a63e6df7a371fe1
Samuel-joseph05 <samuela05022004@gmail.com>
2025-12-06 14:48:44 +0530

    feat: remove unused global styles and main application files

--- src/pages/index.jsx
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div className="text-center place-items-center mt-20">
-      <h1>Welcome to My Next.js App</h1>
-      <p>This is a minimal setup with JavaScript.</p>
-    </div>
-  );
-}
