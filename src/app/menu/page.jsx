cc53fbea5c9a3c78557047b141761453f17e19ba
Samuel-joseph05 <samuela05022004@gmail.com>
2025-12-06 14:47:34 +0530

    feat: add Menubar component for navigation links

---
+++ src/app/menu/page.jsx
@@ -0,0 +1,41 @@
+"use client";
+import { useRouter } from "next/navigation";
+import { HiXMark } from "react-icons/hi2";
+
+export default function Menubar() {
+
+
+  const router = useRouter();
+  const aboutus = () => {
+    router.push("/about_us");
+  };
+  const ProductHighlights = () => {
+    router.push("/product_highlights");
+  };
+  const contact = () => {
+    router.push("/contact");
+  };
+
+
+  return (
+    <section className="">
+      <div className="absolute right-4 top-9">
+        <HiXMark
+          size={30}
+          onClick={() => {
+            router.push("/");
+          }}
+        />
+      </div>
+      <div className="relative font-medium ml-22 mt-25 text-2xl">
+        <p className="mb-3 hover:underline" onClick={aboutus}>
+          About Us
+        </p>
+        <p className="mb-3 hover:underline" onClick={ProductHighlights}>
+          Product Highlights
+        </p>
+        <p onClick={contact} className="hover:underline">Contact Us</p>
+      </div>
+    </section>
+  );
+}
